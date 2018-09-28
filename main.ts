be24560e5d67964ba72623268d4bafc77e8cc515
komaba <43165861+komaba@users.noreply.github.com>
2018-09-28 17:03:44 +0900

    send infrared

--- main.ts
+++ main.ts
@@ -1,7 +1,25 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+//% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
+namespace infrared {
+    //% blockId=on block="ON"
+    export function on(): void {
+        for (let i = 0; i < 4; i++) {
+            pins.analogWritePin(AnalogPin.P0, 512)
+            pins.analogSetPeriod(AnalogPin.P0, 26.3)
+            control.waitMicros(2000)
+            pins.analogWritePin(AnalogPin.P0, 0)
+            control.waitMicros(800)
+        }
+        basic.showString("ON")
+    }
+    //% blockId=off block="OFF"
+    export function off(): void {
+        for (let i = 0; i < 4; i++) {
+            pins.analogWritePin(AnalogPin.P0, 512)
+            pins.analogSetPeriod(AnalogPin.P0, 26.3)
+            control.waitMicros(800)
+            pins.analogWritePin(AnalogPin.P0, 0)
+            control.waitMicros(2000)
+        }
+        basic.showString("OFF")
+    }
+}
