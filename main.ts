f904c99d26b91b456fdd54de077cdf1694519c41
plainbanana <s1820650@s.tsukuba.ac.jp>
2018-10-02 11:37:42 +0900

    Change "on/off" to "send"

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-//% weight=70 icon="\u30A2" color=#EC7505 block="赤外線"
+//% weight=70 icon="" color=#EC7505 block="赤外線"
 namespace infrared {
-    //% blockId=on block="ON"
-    export function on(): void {
+    //% blockId=send block="送信"
+    export function send(): void {
         for (let i = 0; i < 4; i++) {
             pins.analogWritePin(AnalogPin.P0, 512)
             pins.analogSetPeriod(AnalogPin.P0, 26.3)
@@ -9,17 +9,6 @@ namespace infrared {
             pins.analogWritePin(AnalogPin.P0, 0)
             control.waitMicros(800)
         }
-        basic.showString("ON")
-    }
-    //% blockId=off block="OFF"
-    export function off(): void {
-        for (let i = 0; i < 4; i++) {
-            pins.analogWritePin(AnalogPin.P0, 512)
-            pins.analogSetPeriod(AnalogPin.P0, 26.3)
-            control.waitMicros(800)
-            pins.analogWritePin(AnalogPin.P0, 0)
-            control.waitMicros(2000)
-        }
-        basic.showString("OFF")
+        basic.showString("SEND")
     }
 }
